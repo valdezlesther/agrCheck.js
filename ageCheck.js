8b3c26c14fa0f196698f41b9b3229e33678c40e3
valdezlesther <marklesthervaldez52@gmail.com>
2024-10-10 14:00:38 +0800

    Create ageCheck.js

---
+++ ageCheck.js
@@ -0,0 +1,8 @@
+let age = prompt("Enter your age:");
+
+if (age < 13
+ {console.log("You are a child.");}
+ {else if (age >= 13 && age <= 19) 
+ {console.log("You are a teenager.");}
+ else 
+  {console.log("You are an adult.");}
